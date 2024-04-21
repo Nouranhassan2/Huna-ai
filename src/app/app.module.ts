4182c7ba9892d906c28f4fc3569fc57007209788
Nouranhassan2 <nouranhassan618@gmail.com>
2024-04-21 14:01:58 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,9 +13,13 @@ import { ShowCaseComponent } from './components/show-case/show-case.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { FeaturesComponent } from './components/features/features.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [AppComponent, HomeComponent, DetailsComponent, NotfoundComponent, NavbarComponent, ShowCaseComponent, HeroComponent, FeaturesComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgbModule],
+  declarations: [AppComponent, HomeComponent, DetailsComponent, NotfoundComponent, NavbarComponent, ShowCaseComponent, HeroComponent, FeaturesComponent, AboutUsComponent, ContactUsComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgbModule,ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/contact-us/contact-us.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact-us',
+  templateUrl: './contact-us.component.html',
+  styleUrls: ['./contact-us.component.css']
+})
+export class ContactUsComponent {
+
+  contactForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', [Validators.required])
+  });
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form Value', this.contactForm.value);
+      // Here you can add the logic to send the form data to a server or email service.
+    }
+  }
+}
