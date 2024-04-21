6664563c10aea11674151274d7cdab94aeb92df1
Nouranhassan2 <nouranhassan618@gmail.com>
2024-04-21 14:21:43 +0200

    first commit

--- src/app/components/features/features.component.ts
+++ src/app/components/features/features.component.ts
@@ -9,19 +9,19 @@ export class FeaturesComponent {
 
   items = [
     {
-      icon: 'assets/freepik--Android--inject-39.jpg',
+      icon: 'assets/card1.svg',
       title: 'مقالات',
       description: 'لإثراء معلوماتك عن الذكاء الاصطناعي وكل جديد فيه، يمكنك قراءة مقالاتنا عن كيفية استخدام الذكاء وفائدته لمجتمعنا.',
       buttonText: 'اطلع على المقالات'
     },
     {
-      icon: 'assets/freepik--Robot--inject-54.jpg',
+      icon: 'assets/card2.svg',
       title: 'تلقينات الذكاء الاصطناعي',
       description: 'وفرنا لك تلقينات جاهزة للاستخدام في معظم المجالات لاستخراج أفضل النتائج من الذكاء الاصطناعي',
       buttonText: 'ابدأ الاستخدام'   
      },
     {
-      icon: 'path/to/icon1.png',
+      icon: 'assets/card3.svg',
       title: 'أدوات الذكاء الاصطناعي',
       description:'جمعنا لك أكثر من 5000 أداة في مكان واحد لتسهل عليك عملية البحث عن الأداة التي تحتاجها في مجالات مختلفة',
       buttonText:  'ابدأ البحث عن أداة'
